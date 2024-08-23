bbdf8d3255c71a9ddaba90086b74b6d4e60d3724
German Moreno <grmmrno@gmail.com>
2024-08-23 12:00:53 -0400

    Iniciando con Redux

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,32 @@
+import { IconButton } from "@mui/material"
+
+import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
+import { NoteView } from "../views/NoteView"
+import { AddOutlined } from "@mui/icons-material"
+
+
+export const JournalPage = () => {
+    return (
+        <JournalLayout>
+            {/* <Typography></Typography> */}
+            <NothingSelectedView />
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+
+                <AddOutlined />
+            </IconButton>
+            {/* <NoteView /> */}
+        </JournalLayout>
+    )
+}
---
+++ src/journal/routes/JournalRoutes.jsx
@@ -0,0 +1,15 @@
+import { Route } from "react-router-dom"
+import { Routes } from "react-router-dom"
+import { JournalPage } from "../pages/JournalPage"
+import { Navigate } from "react-router-dom"
+
+
+export const JournalRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<JournalPage />} />
+
+            <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+    )
+}
